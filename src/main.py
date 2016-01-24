68cd71a24362bfa03b0798f6de8f6388c18612c3
k0pernicus <antonin.carette@gmail.com>
2016-01-24 18:27:53 +0100

    Documentation de l'exemple

--- src/main.py
+++ src/main.py
@@ -5,15 +5,18 @@ from lib.rule import Rule
 
 def test():
 
+    # Création des noeuds
     uno = Node("uno")
     dos = Node("dos")
     tres = Node("tres")
     quatro = Node("quatro")
 
+    # Création des relations
     r = Relation("R", directed = True)
     s = Relation("S", directed = True)
     t = Relation("T", directed = True)
 
+    # Ajout des noeuds dans les relations, et création des arêtes
     r.add_node(uno)
     r.add_node(dos)
     r.add_node(quatro)
@@ -30,23 +33,31 @@ def test():
     t.add_edge((tres, quatro))
     t.add_edge((quatro, uno))
 
+    # Création du graph, qui contient la liste des noeuds créés et la liste des relations utilisant les noeuds
     g = Graph("graph", [uno, dos, tres, quatro], [r, s, t])
 
+    # Méthode permettant de sortir toutes les caractéristiques du graphe
     g.print_all()
 
+    # Création des règles!
+    # Définition d'une règle!
     pr_0 = Rule("pr_0", ["x", "y"], [(r, ("x", "z")), (s, ("z", "y"))])
-
+    # Définition du goal, utilisant la règle pr_0!
     goal = Rule("Goal", ["x", "y"], [(pr_0, ("x","y"))], goal=True)
 
+    # Ajout des règles dans une liste, afin de pouvoir la passer au graphe pour la résolution
     rules = [pr_0, goal]
 
+    # Impression des règles
     print("Rules:")
     for r in rules:
         print("\t* {}".format(r))
-
     print("")
 
+    # Résolution du problème
     solutions = g.resolve(rules)
+
+    # Impression des solutions
     if len(solutions) != 0:
         print("Solutions:")
         for s in solutions:
